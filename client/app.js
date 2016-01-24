df3edec4988ade4419ecf57ff0e940ae76c69ed0
Tom Fitzpatrick <tfitz237@gmail.com>
2016-01-24 06:30:06 +0000

    Initiated Posts model interaciton. Style, embed, and layout created

--- client/app.js
+++ client/app.js
@@ -1,19 +1,91 @@
-angular.module('app', ['angular-meteor'])
-.controller('PostCtrl', PostCtrl);
+angular.module('app', ['angular-meteor','ngSanitize'])
+.controller('PostCtrl', PostCtrl)
+.directive('dropdown', dropdownDir);
+PostCtrl.$inject = ['$scope', '$meteor','$sce', '$http'];
 
-PostCtrl.$inject = ['$scope', '$meteor'];
-
-function PostCtrl($scope, $meteor) {
+function PostCtrl($scope, $meteor,$sce, $http) {
 	
-	//$scope.posts = $meteor.collection(Post);
-	$scope.posts = [
-		{ 
-			id: '1',
-			title: 'First Post',
-			text: 'lorem ipsum this is my first blog post',
-			image: 'https://lh3.googleusercontent.com/5Qe5qAnblFwtkSzJZRkl4pSDJ4qtkcqvIzVzhLO6bmtkLYzjymwQA7xdyFMMmvU0USEvdCn9RG6L8jgvduS9WQcxQwC3vsIX7tuIIX655XryyhtBT0AYNnk7KWSqLnm-n94caAPW6i40Ip9lsUHrGqKiCTedQ3AE71hp5HEWbhQHAWpDRDsLTvgTdrO8nbwnWs9kCvh5PeOFCsIDunS6OrmwxhteHzqqpiJtCqTd5zsxLkC5C3phUR07PvkD19k_uF6w7BO38u6jNsgfR66umwDBvE-1EiVSxAFwF8RwDv8fpbS3daSP5kni6BWclJpwH-u_C4qt9dohyFrYEawa92AKHvLVSa944RNqkdLIJQ6pVNTPYSj1JmK9vxmJte64n5U4mX34d_ZE6x4xmFAzbI9ce391iAygIqROCJ2NBLJRvZYSqZuzN0hUeJn2N-WqqA9KsoDjORsvk6mDjHAurokgCKhJl22R_X24kBHDNd_HxcyhIZuNYhIaxj6SHFimQelqHp8cJNMUb0FZv5GY4OOo19D5CvICaW-zZGkgrF1AkY2qz6aDkqKY_eqCmVJIBpZ5=w381-h677-no'
- 		},
+	
+	$scope.posts = $meteor.collection(Posts);
+	console.log($scope.posts);
+	$scope.addPost = addPost;
+	$scope.embed = embed;
+	$scope.editPost = editPost;
+	$scope.deletePost = deletePost;
+	$scope.newPost = {};
+	function embed(embed) {
+		return $sce.trustAsHtml(embed.html);
+		
+		
+	}
+	
+	function addPost(type, post) {
+		if(post._id == undefined) {
+			post.type = type;
+			if(post.type == 'music') {
+			$http({method: 'GET', url: 'https://api.embedly.com/1/oembed?key=afc044c2a50440c0bd8216adddc728f2&url='+post.url})
+				.then(function successCallBack(response) {
+				post.embed = response.data;
+				$scope.posts.push(post);
+				}, function errorCallback(response) {
+					console.log(response);
+				});
+				$scope.newPost[type] = {};
+			} else {
+				$scope.posts.push(post);
+			}
+		}
+		else {
+			if(post.type == 'music') {
+			$http({method: 'GET', url: 'https://api.embedly.com/1/oembed?key=afc044c2a50440c0bd8216adddc728f2&url='+post.url})
+				.then(function successCallBack(response) {
+				post.embed = response.data;
+				for (var i = 0; i < $scope.posts.length; i++) {
+					if($scope.posts[i]._id == post._id) {
+						$scope.posts[i] = post;
+						$scope.newPost[type] = {};
+					}
+				}
+				}, function errorCallback(response) {
+					console.log(response);
+				});
 
+			} else {
+				for (var i = 0; i < $scope.posts.length; i++) {
+					if($scope.posts[i]._id == post._id) {
+						$scope.posts[i] = post;
+						$scope.newPost[type] = {};
+					}
+				}
+			}
+			
+		}
+	}
+	
+	function editPost(post) {
+		$scope.newPost[post.type] = post;
+		$('#modal-'+post.type).openModal();
+		
+		
+	}
+	
+	function deletePost(post) {
+		
+		$scope.posts.remove(post._id);
+	}
+	
+	
 
-	];
+}
+
+function dropdownDir() {
+	return {
+		template: function(elem, attr) {
+			return '<a class="post-menu-link dropdown-button white-text" href="#" data-activates="post-menu-'+attr.id+'"><i class="material-icons">more_vert</i></a>';
+		}	
+	};
+	
+	
+	
+	
 }
